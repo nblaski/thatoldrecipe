4457dab96f8ec5f891450951dd4bd7d8192393f9
nblaski <nfb@nicolebruno.com>
2022-05-19 15:51:23 -0400

    testing

--- s3Service.js
+++ s3Service.js
@@ -5,15 +5,15 @@ const uuid = require('uuid').v4;
 
 
 
-exports.s3Uploadv2 = async (files) => {
-    const s3 = new S3();
+//single file upload
+exports.s3Uploadv2 = async (file) => {
+    const s3 = new S3()
 
-    const params = files.map(file => {
-        return {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: `uploads/${uuid()}-${file.originalname}`,
-            Body: file.buffer
-        };
-    })
-    return await Promise.all(params.map(param => s3.upload(param).promise()))
+    const param = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `uploads/${uuid()}-${file.originalname}`,
+        // Body = buffer
+        Body: file.buffer
+    };
+    return await s3.upload(param).promise();
 }
