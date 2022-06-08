edbbc6191e5bdb6fd385011d4ec285c8160b1517
nblaski <nfb@nicolebruno.com>
2022-06-08 00:24:09 -0400

    working on new form

--- public/js/inputs.js
+++ public/js/inputs.js
@@ -3,6 +3,9 @@ let form = document.forms["newForm"];
 
 let nameInput = document.getElementById("name");
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     autosize(document.querySelectorAll('#story'));
+// }, false);
 
 function addField(element, input){
     // Stopping the function if the input field is empty
@@ -14,10 +17,21 @@ function addField(element, input){
     let div = document.createElement("div");
     div.setAttribute("class", "field");
 
-    // Creating the input element
-    let field = document.createElement("input");
-    field.setAttribute("type", "text");
-    field.setAttribute("name", `${input.id}[]`);
+    let field;
+    if(input.id === "stepName") {
+        // Creating the input element
+        field = document.createElement("textarea");
+        field.setAttribute("rows", 4);
+        field.setAttribute("cols", 50);
+        field.setAttribute("id", "stepName");
+        field.setAttribute("name", `${input.id}[]`);
+    } else {
+        field = document.createElement("input");
+        field.setAttribute("type", "text");
+        field.setAttribute("id", "ingredients");
+        field.setAttribute("name", `${input.id}[]`);
+    }
+    
 
     // Creating the span  plus element
 
@@ -32,10 +46,23 @@ function addField(element, input){
     let minusText = document.createTextNode("-");
     minus.appendChild(minusText);
 
+
+    const countAll = document.querySelectorAll('#stepName').length;
+    console.log(countAll);
+
+    let stepHeader = document.createElement("h6");
+    stepHeader.setAttribute("style", "width: 100%");
+    stepHeader.setAttribute("id", "stepHeader");
+
+  
+    stepHeader.innerHTML = `Step ${countAll}`
+
+
     let inputDiv = document.getElementById(input.id)
 
     // Adding the elements to the DOM
     inputDiv.insertBefore(div, inputDiv.lastChild);
+    div.appendChild(stepHeader);
     div.appendChild(field);
     div.appendChild(plus);
     div.appendChild(minus);
@@ -47,6 +74,70 @@ function addField(element, input){
     // Hiding the plus sign
     element.style.display = "none";
 }
+
+
+
+function addLine(element, input){
+    // Stopping the function if the input field is empty
+    if(element.previousElementSibling.value.trim() === ""){
+        return false;
+    }
+    console.log("activated");
+
+    // Creating the div container
+    let li = document.createElement("li");
+    li.setAttribute("class", "field");
+
+    let field;
+    if(input.id === "stepOL") {
+        // Creating the input element
+        field = document.createElement("textarea");
+        field.setAttribute("rows", 4);
+        field.setAttribute("cols", 50);
+        field.setAttribute("id", "stepNameOL");
+        field.setAttribute("name", `${input.id}[]`);
+    } else {
+        field = document.createElement("input");
+        field.setAttribute("type", "text");
+        field.setAttribute("id", "ingredients");
+        field.setAttribute("name", `${input.id}[]`);
+    }
+    
+
+    // Creating the span  plus element
+
+    let plus = document.createElement("span");
+    plus.setAttribute("onclick", `addLine(this, getElementById('${input.id}'))`);
+    let plusText = document.createTextNode("+");
+    plus.appendChild(plusText);
+
+    // Creating the span minus element
+    let minus = document.createElement("span");
+    minus.setAttribute("onclick", "removeField(this)");
+    let minusText = document.createTextNode("-");
+    minus.appendChild(minusText);
+
+    let inputOL = document.getElementById(input.id)
+
+    // Adding the elements to the DOM
+    inputOL.insertBefore(li, inputOL.lastChild);
+    li.appendChild(field);
+    li.appendChild(plus);
+    li.appendChild(minus);
+
+
+    // Un hiding the minus sign
+    element.nextElementSibling.style.display = "block";
+
+    // Hiding the plus sign
+    element.style.display = "none";
+}
+
+
+
+
+
+
 
 // Remove element function
 function removeField(element){
