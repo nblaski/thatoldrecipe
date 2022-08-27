8645be2309111a7e0d2c86ebf39033dcb7a83c1a
Nicole Laski <nfb@nicolebruno.com>
2022-08-27 14:51:49 -0400

    git test

--- appDev.js
+++ appDev.js
@@ -21,7 +21,7 @@ const app = express();
 // PASSPORT CONFIG
 require('./config/passport')(passport);
 
-// CONNECT TO MongoDB
+// CONNECT TO MongoDB THIS IS A GIT TEST
 mongoose.connect(process.env.DEV_DATABASE, {
     useNewUrlParser : true,
     useUnifiedTopology : true,
